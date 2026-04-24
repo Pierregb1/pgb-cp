ff6f19185f0c42442dd77bc712290188724d2287
Pierregb1 <pierre.garciab@gmail.com>
2026-04-24 17:48:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/pdfs", express.static("pdfs"));
+app.use(express.static(__dirname));
 
 app.use(session({
   secret: process.env.SESSION_SECRET || "dev_secret",
