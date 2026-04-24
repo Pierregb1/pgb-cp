f14bda442760f4ed9844d5082bac3b8e0d295d95
Pierregb1 <pierre.garciab@gmail.com>
2026-04-24 16:44:28 +0200

    Update create-student.js

--- scripts/create-student.js
+++ scripts/create-student.js
@@ -1,34 +1,35 @@
-
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
-await pool.query(`
-  CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    email TEXT UNIQUE,
-    password TEXT
-  );
-`);
+
 const [,, email, password] = process.argv;
 
 async function run() {
-  if (!email || !password) {
-    console.log("Usage: node scripts/create-student.js email password");
-    process.exit(1);
-  }
+
+  // création table
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      email TEXT UNIQUE,
+      password TEXT
+    );
+  `);
+
   const hash = await bcrypt.hash(password, 10);
+
   await pool.query(
-    "INSERT INTO users(email, password) VALUES($1,$2) ON CONFLICT (email) DO NOTHING",
+    "INSERT INTO users(email, password) VALUES($1,$2)",
     [email, hash]
   );
-  console.log("Utilisateur créé ou déjà existant");
+
+  console.log("Utilisateur créé");
+
   process.exit();
 }
 
+// ⚠️ IMPORTANT
 run();
