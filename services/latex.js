6bd29b0e42dde262cdd1e82ca36a9a498e89dbad
Pierregb1 <pierre.garciab@gmail.com>
2026-04-24 21:39:46 +0200

    Update latex.js

--- services/latex.js
+++ services/latex.js
@@ -1,16 +1,39 @@
 const { exec } = require("child_process");
 
-function compileLatex(folder) {
+function run(cmd, cwd) {
   return new Promise((resolve, reject) => {
-    exec(
-      "pdflatex -interaction=nonstopmode main.tex",
-      { cwd: folder },
-      (err, stdout, stderr) => {
-        if (err) return reject(stderr);
-        resolve(stdout);
-      }
-    );
+    exec(cmd, { cwd }, (err, stdout, stderr) => {
+      if (err) return reject(stdout + "\n" + stderr);
+      resolve(stdout + "\n" + stderr);
+    });
   });
+}
+
+async function compileLatex(folder) {
+  let logs = "";
+
+  try {
+    // 1ère compilation
+    logs += await run("pdflatex -interaction=nonstopmode main.tex", folder);
+
+    // Bibtex (si présent)
+    try {
+      logs += await run("bibtex main", folder);
+    } catch (e) {
+      logs += "\n(no bibtex)\n";
+    }
+
+    // 2ème compilation
+    logs += await run("pdflatex -interaction=nonstopmode main.tex", folder);
+
+    // 3ème compilation (refs)
+    logs += await run("pdflatex -interaction=nonstopmode main.tex", folder);
+
+    return logs;
+
+  } catch (err) {
+    throw logs + "\n" + err;
+  }
 }
 
 module.exports = { compileLatex };
